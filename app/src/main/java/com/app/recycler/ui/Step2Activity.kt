ed7ccedfcef48ff9cc2ab0730f9b6631ce864a5a
ablyUmeshKumar <umesh.kumar.sharma@fatbit.in>
2022-08-12 11:32:39 +0530

    Local commit

--- app/src/main/java/com/app/recycler/ui/Step2Activity.kt
+++ app/src/main/java/com/app/recycler/ui/Step2Activity.kt
@@ -27,14 +27,30 @@ class Step2Activity : AppCompatActivity(), ListingItemClick {
 
         daily_weekly_button_view.setOnCheckedChangeListener { _, checkedId ->
             val radio: RadioButton = findViewById(checkedId)
+            /*if (checkedId==1){
+                radio_yes
+            }*/
             when (radio) {
                 radio_yes -> {
                     // some code
+                    radio_yes.setBackgroundDrawable(resources.getDrawable(R.drawable.btn_enable_bg))
+                    radio_yes.setTextColor(resources.getColor(R.color.white))
+
+                    radio_no.setBackgroundDrawable(resources.getDrawable(R.drawable.btn_white_bg))
+                    radio_no.setTextColor(resources.getColor(R.color.black))
+
+
                     lay_chb.visibility=View.VISIBLE
                     layedt.visibility=View.VISIBLE
                 }
                 radio_no -> {
                     // some code
+                    radio_no.setBackgroundDrawable(resources.getDrawable(R.drawable.btn_enable_bg))
+                    radio_no.setTextColor(resources.getColor(R.color.white))
+
+                    radio_yes.setBackgroundDrawable(resources.getDrawable(R.drawable.btn_white_bg))
+                    radio_yes.setTextColor(resources.getColor(R.color.black))
+
                     layedt.visibility=View.GONE
                     lay_chb.visibility=View.VISIBLE
                 }
@@ -45,24 +61,24 @@ class Step2Activity : AppCompatActivity(), ListingItemClick {
 
 
     fun getGridList(): ArrayList<DummyData> = arrayListOf(
-        DummyData(getString(R.string.txtchb_external), R.drawable.total_survey,""),
-        DummyData(getString(R.string.txtchb_training), R.drawable.total_survey,""),
-        DummyData(getString(R.string.txtchb_workshop), R.drawable.total_survey,""),
-        DummyData(getString(R.string.txtchb01), R.drawable.total_survey,""),
-        DummyData(getString(R.string.txtchb02), R.drawable.in_progress,""),
-        DummyData(getString(R.string.txtchb03), R.drawable.completed,""),
-        DummyData(getString(R.string.txtchb04), R.drawable.delayed,""),
-        DummyData(getString(R.string.txtchb05), R.drawable.all_risk,""),
-        DummyData(getString(R.string.txtchb06), R.drawable.all_risk,""),
-        DummyData(getString(R.string.txtchb07), R.drawable.all_risk,""),
-        DummyData(getString(R.string.txtchb08), R.drawable.all_risk,""),
-        DummyData(getString(R.string.txtchb09), R.drawable.all_risk,""),
-        DummyData(getString(R.string.txtchb10), R.drawable.all_risk,""),
-        DummyData(getString(R.string.txtchb11), R.drawable.all_risk,""),
-        DummyData(getString(R.string.txtchb12), R.drawable.all_risk,""),
-        DummyData(getString(R.string.txtchb13), R.drawable.all_risk,""),
-        DummyData(getString(R.string.txt_chekbox17), R.drawable.all_risk,""),
-        DummyData(getString(R.string.unmute), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_1), R.drawable.total_survey,""),
+        DummyData(getString(R.string.txt_2), R.drawable.total_survey,""),
+        DummyData(getString(R.string.txt_3), R.drawable.total_survey,""),
+        DummyData(getString(R.string.txt_4), R.drawable.total_survey,""),
+        DummyData(getString(R.string.txt_5), R.drawable.in_progress,""),
+        DummyData(getString(R.string.txt_6), R.drawable.completed,""),
+        DummyData(getString(R.string.txt_7), R.drawable.delayed,""),
+        DummyData(getString(R.string.txt_8), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_9), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_10), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_11), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_12), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_13), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_14), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_15), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_16), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_17), R.drawable.all_risk,""),
+        DummyData(getString(R.string.txt_18), R.drawable.all_risk,""),
     )
 
     override fun clickIten(pos: Int) {
